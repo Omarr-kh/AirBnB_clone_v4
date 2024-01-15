ab0002ae866b21132df77abce57459f5ae74e69b
Omarr-kh <omar1752003@gmail.com>
2024-01-14 22:32:36 -0800

    task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  /* filtering amenities */
+  let amenitiesChecked = [];
+
+  function filterAmenities() {
+    amenitiesChecked = amenitiesChecked.filter((item) => item !== '');
+    if (amenitiesChecked.length > 1) {
+      let amenities = amenitiesChecked.join(', ');
+      if (amenities.length > 40) {
+        amenities = amenities.slice(0, 39) + '...';
+      }
+      $('.amenitiesChecked').text(amenities);
+    } else if (amenitiesChecked.length === 1) {
+      $(".amenitiesChecked").text(amenitiesChecked[0]);
+    } else {
+      $(".amenitiesChecked").text("");
+    }
+  }
+
+  $(".amenity-checkbox").on("change", function () {
+    const amenityName = $(this).attr("data-name");
+    if ($(this).prop("checked")) {
+      amenitiesChecked.push(amenityName);
+      filterAmenities();
+    } else {
+      amenitiesChecked.splice(amenitiesChecked.indexOf(amenityName), 1);
+      filterAmenities();
+    }
+  });
+});
